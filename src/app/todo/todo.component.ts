d72e8c165bc1f352f0dc62976041cfdaa2685741
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2019-11-24 16:03:20 -0800

    added code to todo.ts

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../todo.service';
+import { Todo } from '../todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent {
 
-  constructor() { }
+  newTodo: Todo = new Todo();
 
-  ngOnInit() {}
+  constructor(private _todoServe: TodoService) {
+
+  }
+
+
+  addTodo() {
+    if (!this.newTodo.title) {
+      return;
+    }
+
+    this._todoServe.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this._todoServe.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this._todoServe.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this._todoServe.getAllTodos();
+  }
 
 }
