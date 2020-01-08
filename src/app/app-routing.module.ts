334bd9fcc14f53ae18f400fb186fa16cd4da0860
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2020-01-08 14:19:31 -0800

    menu updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { TodoComponent } from './todo/todo.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'todo', pathMatch: 'full' },
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: 'home', redirectTo: 'todo', pathMatch: 'full'},
   { path: 'todo', component: TodoComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'menu', component: MenuComponent}
 ];
 
 @NgModule({
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule, ActivatedRoute } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent {
 
-  constructor() { }
+  constructor(private router: Router, private actRoute: ActivatedRoute) { }
 
 
 }
